70cbff169dd4e1a1edd6881d9615ee4c63ad134d
Thyago Nunes <devthyagonunes@gmail.com>
2022-04-04 23:11:06 -0300

    -R contact teste from contacts

--- src/mocks/contacts.js
+++ src/mocks/contacts.js
@@ -18,12 +18,4 @@ module.exports = [
     category_id: v4(),
   },
 
-  {
-    id: v4(),
-    name: 'Teste',
-    email: 'teste@mail.com',
-    phone: '(99)99999-9999',
-    birth: 'xx-xx-19xx',
-    category_id: v4(),
-  },
 ];
